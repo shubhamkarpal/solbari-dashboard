54337f3b03aa00dd3eebdf4de7d9b9cd2c3119b8
shubhamkarpal <shubhamkar.pal@pattern.com>
2026-04-29 11:16:21 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const CONFIG = { APPS_SCRIPT_URL: "YOUR_APPS_SCRIPT_DEPLOYMENT_URL_HERE" };
+const CONFIG = { APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw6X1VcGIJPV3o4BJx0iGB6A-VrqnnNREC_YLjsH3nWUVFe-korqWKZr63vMWchJTPx/exec" };
